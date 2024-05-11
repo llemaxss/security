7ac8a4d3b0b524b95b1380ad1d689980425b35f3
llemaxss <llemaxiss@gmail.com>
2024-05-11 12:01:27 +0400

    remove empty rows

--- src/main/java/com/gmail/llemaxiss/security/SecurityApplication.java
+++ src/main/java/com/gmail/llemaxiss/security/SecurityApplication.java
@@ -2,13 +2,10 @@ package com.gmail.llemaxiss.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class SecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
-
 }
